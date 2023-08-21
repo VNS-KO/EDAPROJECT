a6965cc362f5c8cefaf94e7edca4fb05235bd9cf
VNS-KO <79849717+VNS-KO@users.noreply.github.com>
2023-08-21 17:07:41 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
-#initiating a
-a=5
-#initiating b
-b=6
-#printing addition of a and b
-print(a+b)
+from matplotlib import pyplot as plt    
+#ploting our canvas    
+plt.plot([1,2,3],[4,5,1])    
+#display the graph    
+plt.show()    
