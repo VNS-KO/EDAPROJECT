33576af6b6c9601d1a12dd163121b69923555e5b
VNS-KO <79849717+VNS-KO@users.noreply.github.com>
2023-08-22 12:30:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,27 +1,2 @@
-def fibonacci(n, memo={}):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n in memo:
-        return memo[n]
-    else:
-        memo[n] = fibonacci(n-1) + fibonacci(n-2)
-        return memo[n]
- 
-print(fibonacci(9))
-
-# Define a list of participant
-participant = ['Monalisa', 'Akbar Hossain',
-               'Jakir Hasan', 'Zahadur Rahman', 'Zenifer Lopez']
-
-# Define the function to filters selected candidates
-def SelectedPerson(participant):
-    selected = ['Akbar Hossain', 'Zillur Rahman', 'Monalisa']
-    if (participant in selected):
-        return True
-
-selectedList = filter(SelectedPerson, participant)
-print('The selected candidates are:')
-for candidate in selectedList:
-    print(candidate)
+import streamlit as st
+st.title("Demo")
